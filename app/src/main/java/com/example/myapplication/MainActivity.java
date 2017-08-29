28732c03f306bcdaf63a5f658ed308c4d74fb0c5
Vladimir Nechaev <vovan.nechaev19@yandex.ru>
2017-08-29 19:07:10 +0400

    Add video

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,33 +2,47 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
+    VideoView videoView;
 
+    static final int REQUEST_VIDEO_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnCamera = (Button) findViewById(R.id.btnCamera);
+        Button btnPhoto = (Button) findViewById(R.id.btnPhoto);
         imageView = (ImageView) findViewById(R.id.ImageViev);
 
-        btnCamera.setOnClickListener(new View.OnClickListener() {
+        Button btnVideo = (Button) findViewById(R.id.btnVideo);
+        videoView = (VideoView) findViewById(R.id.VideoViev);
+
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View PhotoView) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent,0);
             }
         });
+    }
+
+    public void dispatchTakeVideoIntent(View VideoView) {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
     }
 
     @Override
@@ -36,5 +50,13 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         Bitmap bitmap = (Bitmap)data.getExtras().get("data");
         imageView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = intent.getData();
+            mVideoView.setVideoURI(videoUri);
+        }
     }
 }
